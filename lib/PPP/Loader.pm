880b910f825542e167ec532e2aeca7fdda9ccdb3
Peter Stuifzand <peter@stuifzand.eu>
2013-06-14 20:07:18 +0200

    Move loader code to PPP::Loader

---
+++ lib/PPP/Loader.pm
@@ -0,0 +1,48 @@
+package PPP::Loader;
+use Moo;
+
+use PPP::Parser;
+use PPP::Toplevel;
+use Data::Dumper;
+use Scalar::Util 'blessed';
+
+use Class::Load 'load_class';
+
+sub load {
+    my ($self, $string) = @_;
+
+    my $ppp = PPP::Parser->new;
+
+    my $ast = $ppp->parse($string);
+
+    $self->_load_classes($ast);
+
+    my $toplevel = PPP::Toplevel->new;
+    my $scope = $toplevel;
+
+    for my $d ($ast->decls) {
+        my $keyword = $d->keyword->name;
+        my $name    = $d->name->name;
+        $scope->process($keyword, $name, $d->arguments->as_list, $d->block);
+    }
+
+    return $scope;
+}
+
+sub _load_classes {
+    my $self = shift;
+    my $ast = shift;
+
+    my $class = blessed($ast);
+
+    if ($class) {
+        load_class($class);
+
+        for (@$ast) {
+            $self->_load_classes($_);
+        }
+    }
+}
+
+1;
+
--- test.pl
+++ test.pl
@@ -1,17 +1,13 @@
 #!/usr/bin/env perl
 use 5.10.0;
 use lib 'lib';
-use Marpa::R2;
-use PPP::Parser;
-use PPP::Toplevel;
+
+use PPP::Loader;
 use Data::Dumper;
-use Scalar::Util 'blessed';
 
-use Class::Load 'load_class';
+my $loader = PPP::Loader->new;
 
-my $ppp = PPP::Parser->new;
-
-my $ast = $ppp->parse(<<'PPP');
+my $toplevel = $loader->load(<<'PPP');
 class Point {
     has $x (is => rw, default => 0);
     has $y (is => rw, default => 0);
@@ -24,33 +20,6 @@ class Person {
     has $age;
 }
 PPP
-
-#print Dumper($ast);
-load_classes($ast);
-
-sub load_classes {
-    my $self = shift;
-
-    my $class = blessed($self);
-    if ($class) {
-        load_class($class);
-    }
-
-    for (@$self) {
-        load_classes($_);
-    }
-}
-
-my $toplevel = PPP::Toplevel->new;
-my $scope = $toplevel;
-
-for my $d ($ast->decls) {
-    my $keyword = $d->keyword->name;
-    my $name    = $d->name->name;
-    $scope->process($keyword, $name, $d->arguments->as_list, $d->block);
-}
-
-$scope->dump_classes;
 
 {
     my $class = $toplevel->find_class('Point');
